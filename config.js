6110ba4205485b7a9f5c63d7ca55eb56b6e760a5
Jan Jakubas <niko@libunity.org>
2023-10-01 07:25:37 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+
+const dtdd_api_enabled = true;
+const dtdd_api_key = "your_api_key_here"; // Replace with your actual API key
+const use_memcache = false;
+const memcache_address = "localhost";
+const memcache_port = 11211;
+const invalidation_time = 86400; // 24 hours
+const use_dtdd_web_api = true;
+const dtdd_web_api_address = "http://dtdd.valknight.xyz";
+const use_short_names = false;
+const dtdd_api_key = "example"; // Replace with your actual API key
+const dtdd_api_enabled = true;
+
+export { dtdd_api_enabled, dtdd_api_key, use_memcache, memcache_address, memcache_port, invalidation_time, use_dtdd_web_api, dtdd_web_api_address, use_short_names, dtdd_api_key, dtdd_api_enabled };
